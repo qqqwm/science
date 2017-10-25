31e8d3bbef28a5466b7bd7d74bbba640fa4297f3
frswedom <frswedom@gmail.com>
2017-10-25 16:10:53 +0300

    Update max_en_nucleon.py
    closed nucleon_energies
    made more declarative variable names

--- max_en_nucleon.py
+++ max_en_nucleon.py
@@ -1,6 +1,7 @@
 nucleon_energies = open("proton_energies_final_3.txt", 'r')
-max_en = 0
+maximal_energy = 0
 for energy in nucleon_energies:
-    if float(energy) > max_en:
+    if float(energy) > maximal_energy:
         max_en = float(energy)
 print(max_en)
+nucleon_energies.close()
